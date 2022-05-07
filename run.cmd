6ce23f3fe689750f7877b10ed845119a02d5927c
Philipp Lengauer <p.lengauer@gmail.com>
2022-05-07 13:29:47 +0200

    added opentelemetry agent

--- run.cmd
+++ run.cmd
@@ -1,3 +1,9 @@
+SET JAVA_TOOL_OPTIONS=-javaagent:.\opentelemetry-javaagent.jar
+SET OTEL_RESOURCE_ATTRIBUTES=service.name=Dionysus,service.version=1.0.0
+SET OTEL_TRACES_EXPORTER=otlp
+SET OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=https://ldj78075.sprint.dynatracelabs.com/api/v2/otlp/v1/traces
+SET OTEL_EXPORTER_OTLP_TRACES_PROTOCOL=http/protobuf
+SET OTEL_EXPORTER_OTLP_TRACES_HEADERS=Authorization=Api-Token %DIONYSUS_DYNATRACE_API_TOKEN%
 :start
 %JAVA_17_HOME%\bin\java.exe -verbose:gc -Xmx100m -Djava.util.logging.config.file=logging.properties -Dupdate=true -Ddionysus.delay=180 -jar Dionysus.jar default
 goto start
