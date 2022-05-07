4eff46b46f881489af9faff3e15dad1652c960b5
Philipp Lengauer <p.lengauer@gmail.com>
2022-05-07 21:22:17 +0200

    added gay pride as important date

--- src/at/pl/dionysus/TestEffect.java
+++ src/at/pl/dionysus/TestEffect.java
@@ -1,6 +1,6 @@
 package at.pl.dionysus;
 
-import at.pl.dionysus.applications.date.NewYear;
+import at.pl.dionysus.applications.date.GayPride;
 import at.pl.razer.chroma.Effect;
 import at.pl.razer.chroma.EffectPlayer;
 import at.pl.razer.chroma.SDK;
@@ -11,7 +11,7 @@ public class TestEffect {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         try(SDK sdk = new SDK("Razer Chroma SDK RESTful Test Application", "This is a REST interface test application")) {
-            Effect effect = new NewYear();
+            Effect effect = new GayPride();
             try (EffectPlayer player = new EffectPlayer(sdk, effect)) {
                 player.join(1000 * 60);
             }
--- src/at/pl/dionysus/applications/date/ImportantDatesApplication.java
+++ src/at/pl/dionysus/applications/date/ImportantDatesApplication.java
@@ -67,15 +67,17 @@ public class ImportantDatesApplication implements Application {
     }
 
     private Effect getEffect(int month, int day) {
+        // national days
         if (month == 10 && day == 26) return new Austria();
         else if (month == 7 && day == 14) return new France();
         else if (month == 10 && day == 3) return new Germany();
         else if (month == 6 && day == 11) return new /* Russia() */ Ukraine();
+        // important other dates
+        else if (month == 7 && day == 23) return new GayPride();
         else if (month == 12 && day == 24) return new XMas();
         else if (month == 2 && day == 14) return new ValentinesDay();
         else if (month == 10 && day == 31) return new Halloween();
         else if (month == 12 && day == 31 || month == 1 && day == 1) return new NewYear();
-        //TODO gay pride
         return null;
     }
 
